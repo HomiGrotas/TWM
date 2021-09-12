d5a8b7f1bd925bd53f4608e7326a9167f1670e87
NadavShani <shaninadav10@gmail.com>
2021-09-12 17:43:03 +0300

    added lock and wrapper function functions have to wait until the lock is acquired

--- client/communication/chat_client.py
+++ client/communication/chat_client.py
@@ -1,4 +1,25 @@
 from client.communication.communication import conn
+from threading import Lock
+
+
+def network_method(func):
+    """"
+    wrapper method to wait until it's possible to send message to server
+    (meaning, there isn't another function waiting for response from server)
+    """
+    def wrapper_network(self, *args):
+        # wait for another function to end (acquiring lock)
+        self.busy_lock.acquire()
+        try:
+            value_returned = func(self, *args)
+        except Exception as e:
+            raise e
+
+        # release lock
+        self.busy_lock.release()
+        return value_returned
+
+    return wrapper_network
 
 
 class ChatClient:
@@ -6,6 +27,7 @@ class ChatClient:
         self.username = username
         self.room_name = room_name
         self.join_room()    # auto join to the room
+        self.busy_lock = Lock()
 
     def join_room(self):
         """
@@ -24,6 +46,7 @@ class ChatClient:
         response = conn.receive()
         return response
 
+    @network_method
     def exit_room(self):
         """
         send exit room request to server
@@ -41,6 +64,7 @@ class ChatClient:
         response = conn.receive()
         return response
 
+    @network_method
     def send_msg(self, msg: str):
         """
         send msg to server
@@ -63,6 +87,7 @@ class ChatClient:
             response = conn.receive()
             return response
 
+    @network_method
     def load_messages(self):
         """
         send get messages request to server
@@ -77,6 +102,7 @@ class ChatClient:
         response = conn.receive()['response']
         return response
 
+    @network_method
     def load_connected_users(self):
         send_msg_format = \
             {
